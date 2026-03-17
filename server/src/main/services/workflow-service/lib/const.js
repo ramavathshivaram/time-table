c521270726633650beda1a32de809cc7d0ee9dd3
ramavathshivaram <ramavathshiva6300@gmail.com>
2026-03-17 16:03:57 +0530

    fix(docker): optimised the docker images and using nginx served the client

--- server/src/main/services/workflow-service/lib/const.js
+++ server/src/main/services/workflow-service/lib/const.js
@@ -1,6 +1,3 @@
-
-
-
 export const queueConst = {
-   UPDATE_WORKFLOW: "UPDATE_WORKFLOW",
+  UPDATE_WORKFLOW: "UPDATE_WORKFLOW",
 };
---
+++ server/src/test/unit/utils.test.js
@@ -0,0 +1,5 @@
+import {queueConst} from "#utils/const.js";
+
+test("queueConst", () => {
+   expect(queueConst.SEND_EMAIL).toBe("SEND_EMAIL");
+});
